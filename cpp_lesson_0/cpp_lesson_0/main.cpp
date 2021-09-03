f49b6b3f64a8a65bdbf2537ebb23740924656dad
Artyom2302 <89930657+Artyom2302@users.noreply.github.com>
2021-09-03 18:49:34 +0300

    c++ types

--- cpp_lesson_0/cpp_lesson_0/main.cpp
+++ cpp_lesson_0/cpp_lesson_0/main.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main() 
 {
-	cout << "hello,World"<<endl;
+	cout << "hello,World" << endl<<"please enter new value:";
+	int x = 1;
+	float f=0;
+	double d=0;
+	char ch = 'a';
+	bool b = true;
+	cin  >> x ;
+	cout << endl << x + 1
+	<< endl << x * 3
+	 << endl << (double)x/2.<<endl;
+	x += 2;//x=x+2;
+	int a1=x++;//x=x+1;
+	int a2=++x;
+	x = 7;
+	cout << (x >> 1) << endl;
 	return 0;
+	
 }
