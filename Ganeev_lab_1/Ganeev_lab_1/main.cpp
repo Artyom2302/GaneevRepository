b68fa80201cdb1c60e2d0c98e65fd57d58d693dc
Artyom2302 <89930657+Artyom2302@users.noreply.github.com>
2021-09-05 19:40:41 +0300

    Slichkom_clochna_Nevosmoshna

--- Ganeev_lab_1/Ganeev_lab_1/main.cpp
+++ Ganeev_lab_1/Ganeev_lab_1/main.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <clocale>
+#include <iostream>//in out поток
+#include <vector>//работа с динамическими массивами
+#include <sstream>// для работы со строками
+#include <clocale>// для русского языка
+#include  <conio.h>// Для ввода с клавиатуры
+#include <cstring>
+#include <string>
 using namespace std;
+
 //Cоздание структуры Труба
 struct pipe {
 	unsigned int id;
@@ -14,28 +18,122 @@ struct pipe {
 struct compressor {
 	unsigned int id;
 	string name;
-	unsigned int allworkshops;
+	unsigned int workshops;
 	unsigned int workshopsinwork;
 	double performance;
 	
 };
+//Функция проверки
+bool checkint(string s) {
+	if ((s.size() != 0) && (s[0] != '0')) {
+		for (size_t i = 0; i < s.size(); i++)
+		{
+			if (isdigit(s[i]))
+			{
+				continue;
+			}
+			else {
+				return false;
+			}
 
-int main() {
-	setlocale(LC_CTYPE, "rus");//установка русского языка 
-	//cоздание меню
-	cout << "1. Добавить трубу" << endl<<
+		}
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//Создание объекта типа данных труба
+void addpipe(vector <pipe>& pipesf) //pipesf -вектор труб для функции
+{
+	system("cls");
+	pipesf.resize(pipesf.size()+1);
+	//cоздаем переменные типа string для считывания с консоли 
+	string value1;
+	
+	
+	while (true){
+		cout << "Введите ID: ";
+		getline(cin, value1);
+		if (checkint(value1) ){
+			pipesf[pipesf.size() - 1].id = stoi(value1);
+			break;
+		}
+		else
+		{
+			cout << "Введите значение верно !!! \n";
+		}
+	}
+	
+	cout << endl << "Введите длину: " ;
+	cin >> pipesf[pipesf.size()-1].length;
+	cout << endl << "Введите диаметр: ";
+	cin >> pipesf[pipesf.size()-1].diameter;
+	cout << endl << "Введите 1,если труба в ремонте или " ;
+	cout << "введите 0,если труба в пригодном для экплуатации: " ;
+	cin >> pipesf[pipesf.size()-1].repair;
+}
+void mainmenu()
+	{
+	cout << "1. Добавить трубу" << endl <<
 		"2. Добавить КС" << endl <<
 		"3. Просмотр всех объектов" << endl <<
 		"4. Редактировать трубу" << endl <<
 		"5. Редактировать КС" << endl <<
 		"6. Сохранить" << endl <<
 		"7. Загрузить" << endl <<
-		"0. Выход" << endl ;
-	int button;
-	cin >>button ;
-	if (button==0)
-	{
-		exit(0);
+		"0. Выход" << endl;
 	}
+
+int main() {
+	setlocale(LC_CTYPE, "rus");//установка русского языка 
+	//cоздание меню link="http://www.c-cpp.ru/content/getch-getche"
+	int menustatus;
+	bool join,quit;
+	vector <pipe> pipes ;
+	pipes.resize(0);
+	while (true)
+	{
+		system("cls");
+		mainmenu();
+		menustatus = _getch();
+		while (true)
+		{
+			switch (menustatus)
+			{
+			case 49:
+				addpipe(pipes);
+				join = true;
+				break;
+			case 50:
+				break;
+			case 51:
+				break;
+			case 52:
+				break;
+			case 53:
+				break;
+			case 54:
+				break;
+			case 55:
+				break;
+			case 56:
+				break;
+			default:
+				cout << "Нажмите одну из перечисленных выше кнопок!!!";
+				break;
+			}
+			
+			
+		}
+
+		
+	
+	}
+	
+	
+	_getch();
 	return 0;
+	
 }
